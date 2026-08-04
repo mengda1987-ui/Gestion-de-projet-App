192647b1b66297fa74c1bedc30fbb32aed13d104
Meng Da <mengda@mengdemacbook-pro11.home>
2026-08-05 01:14:09 +0200

    bump version to v1.0.19

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -228,7 +228,7 @@ export default function LoginPage() {
       </div>
 
       {/* Version */}
-      <p className="absolute bottom-6 left-1/2 -translate-x-1/2 text-xs text-black font-medium">v1.0.18</p>
+      <p className="absolute bottom-6 left-1/2 -translate-x-1/2 text-xs text-black font-medium">v1.0.19</p>
 
       {/* Background Picker */}
       {showBgPicker && createPortal(
