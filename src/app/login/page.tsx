61db979a85791a7afbcfc63e0c2854baeb2a74ae
Meng Da <mengda@Host-002.lan>
2026-08-05 14:24:13 +0200

    v1.1.11

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -180,7 +180,7 @@ export default function LoginPage() {
       </div>
 
       {/* Version */}
-      <p className="absolute bottom-6 left-1/2 -translate-x-1/2 text-xs text-black font-medium">v1.1.10</p>
+      <p className="absolute bottom-6 left-1/2 -translate-x-1/2 text-xs text-black font-medium">v1.1.11</p>
 
       {/* Background Picker */}
       {showBgPicker && createPortal(
--- src/components/workspace/BoardHome.tsx
+++ src/components/workspace/BoardHome.tsx
@@ -650,7 +650,7 @@ export default function BoardHome() {
 
       {/* Version */}
       <div className="text-center py-2 text-[11px] text-slate-400 shrink-0">
-        v1.1.10
+        v1.1.11
       </div>
     </div>
   );
